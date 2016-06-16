c4a4f38fe9539f62156c180b9c0f256601fe4353
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-06-16 16:55:48 -0300

    initial card

---
+++ src/common/components/Card/Card.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jhonataalmeida on 6/16/16.
+ */
+import React, {Component, PropsType} from 'react';
+
+class Card extends Component {
+	render(){
+		return(
+			<div>Initial Card</div>
+	);
+	}
+}
+
